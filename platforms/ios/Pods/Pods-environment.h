cd9087c7379cfb4091773d88291c88acd6966d91
guolovecha <dong@dongtekiMacBook-Pro.local>
2013-09-18 01:11:22 +0800

    增加reachability   并修改 ios 为5.0

--- platforms/ios/Pods/Pods-environment.h
+++ platforms/ios/Pods/Pods-environment.h
@@ -82,8 +82,8 @@
 // Reachability
 #define COCOAPODS_POD_AVAILABLE_Reachability
 #define COCOAPODS_VERSION_MAJOR_Reachability 3
-#define COCOAPODS_VERSION_MINOR_Reachability 0
-#define COCOAPODS_VERSION_PATCH_Reachability 0
+#define COCOAPODS_VERSION_MINOR_Reachability 1
+#define COCOAPODS_VERSION_PATCH_Reachability 1
 
 // SSZipArchive
 #define COCOAPODS_POD_AVAILABLE_SSZipArchive
