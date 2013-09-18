1e66d4c20645063d8d6e32f2fe8f8117f9f88ce1
Justin Yip <justin.yip@foxmail.com>
2013-09-18 17:28:28 +0800

    add bsl module definition

---
+++ platforms/ios/bsl/Classes/Core/BSLModuleManager.h
@@ -0,0 +1,14 @@
+//
+//  BSLModuleManager.h
+//  bsl
+//
+//  Created by Justin Yip on 9/15/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+//模块管理器，接入应用程序生命周期
+@interface BSLModuleManager : NSObject
+
+@end
---
+++ platforms/ios/bsl/Classes/Core/BSLModuleManager.m
@@ -0,0 +1,13 @@
+//
+//  BSLModuleManager.m
+//  bsl
+//
+//  Created by Justin Yip on 9/15/13.
+//
+//
+
+#import "BSLModuleManager.h"
+
+@implementation BSLModuleManager
+
+@end
