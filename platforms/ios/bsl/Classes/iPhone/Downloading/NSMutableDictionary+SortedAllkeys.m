1a030c6dc73f6996d5e80dffd7dc506e90ae17e5
apple <zhouj8798@gmail.com>
2013-09-26 15:21:43 +0800

    新增本地模块配置如果本地模块不存在直接过滤该模块

--- platforms/ios/bsl/Classes/iPhone/Downloading/NSMutableDictionary+SortedAllkeys.m
+++ platforms/ios/bsl/Classes/iPhone/Downloading/NSMutableDictionary+SortedAllkeys.m
@@ -43,6 +43,15 @@ NSInteger intSort(id num1, id num2, void *context)
     return [[self allKeys] sortedArrayUsingFunction:intSortBaseModuleEnd context:NULL];
 }
 
+-(NSArray *)sortedAllkeysBaseModulePublicEnd{
+    
+    NSArray *array = [[self allKeys] sortedArrayUsingFunction:intSortBaseModuleEnd context:NULL];
+    NSMutableArray *newArray = [[NSMutableArray alloc]initWithCapacity:0];
+    [newArray addObjectsFromArray:array];
+    NSArray *secArray = [newArray sortedArrayUsingFunction:publicSortBaseModuleEnd context:NULL];
+    return secArray;
+    
+}
 NSInteger intSortBaseModuleEnd(id num1, id num2, void *context)
 {
     
@@ -62,5 +71,25 @@ NSInteger intSortBaseModuleEnd(id num1, id num2, void *context)
     
     return NSOrderedSame;
 }
+
+
+NSInteger publicSortBaseModuleEnd(id num1, id num2, void *context)
+{
+    
+    NSString *c1 = (NSString *)num1;
+    
+    NSString *c2 = (NSString *)num2;
+    if([c1 rangeOfString:@"公共"].location != NSNotFound)
+    {
+        return NSOrderedAscending;
+    }
+    if([c2 rangeOfString:@"公共"].location != NSNotFound)
+    {
+        return NSOrderedDescending;
+    }
+    
+    return NSOrderedSame;
+}
+
 
 @end
