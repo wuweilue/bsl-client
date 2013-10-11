90528d7e3e169e62413133599a506ed5ff38e2ae
果冻 <307863536@qq.com>
2013-10-11 09:46:01 +0800

    修改加载模块退出问题
    修改本地模块无法显示问题

--- platforms/ios/bsl/Classes/Plugins/CubeModule/CubeModulePlugin.m
+++ platforms/ios/bsl/Classes/Plugins/CubeModule/CubeModulePlugin.m
@@ -162,7 +162,11 @@
         [jsonCube setObject:[NSNumber numberWithBool:each.hidden] forKey:@"hidden"];
         [jsonCube  setObject:each.releaseNote forKey:@"releaseNote"];
         [jsonCube  setObject:each.category forKey:@"category"];
-        [jsonCube  setObject:[each.iconUrl stringByAppendingFormat:@"?sessionKey=%@&appKey=%@",token,kAPPKey] forKey:@"icon"];
+        if ([each.local length]>0) {
+            [jsonCube  setObject:each.iconUrl forKey:@"icon"];
+        }else{
+            [jsonCube  setObject:[each.iconUrl stringByAppendingFormat:@"?sessionKey=%@&appKey=%@",token,kAPPKey] forKey:@"icon"];
+        }
         [jsonCube  setObject:each.identifier forKey:@"identifier"];
         [jsonCube  setObject:!each.local?@"":each.local forKey:@"local"];
         [jsonCube  setObject:each.name forKey:@"name"];
